f9b6f4f7670db8501212929c354b4522b39e37da
내 이름은 유병훈 <168532676+dbdbddib@users.noreply.github.com>
2025-01-22 08:14:03 +0900

    Create region.java

---
+++ src/main/java/com/example/tjournal/category/region.java
@@ -0,0 +1,22 @@
+package com.example.tjournal.category;
+
+// url {{region}} 확인 후 반환 하게 만들자
+public enum region {
+    seoul,
+    busan,
+    daegu,
+    incheon,
+    gwangju,
+    daejeon,
+    ulsan,
+    sejong,
+    gyeonggi,
+    gangwon,
+    chungbuk,
+    chungnam,
+    jeonbuk,
+    jeonnam,
+    gyeongbuk,
+    gyeongnam,
+    jeju,
+}
